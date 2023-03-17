dec2c56a6cef90d585872422c08178532d670bf9
gharbiamine <gharbiamine5674@gmail.com>
2023-03-17 12:34:50 +0100

    fixed context

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from "react";
 import { List } from "../components/List";
 import { Search } from "../components/Search";
 import { UserContext, UserProvider } from "../contexts/UserContext";
+
 import { UserModel } from "../models/UserModel";
 import { getRepositories } from "../services/SearchService";
 
@@ -10,26 +11,21 @@ export const LandingPage = () => {
   const handleUsernameSearch = (username: string) => {
     getRepositories(username)
       .then((user: UserModel) => {
-        console.log(user);
         setCurrentUser(user);
-        console.log(currentUser);
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
-  useEffect(() => console.log(currentUser), [currentUser]);
   return (
     <div>
-      <UserProvider>
-        <Search
-          handleSearch={handleUsernameSearch}
-          title="Get github repositories by username"
-          button="Fetch"
-        />
-        <List />
-      </UserProvider>
+      <Search
+        handleSearch={handleUsernameSearch}
+        title="Get github repositories by username"
+        button="Fetch"
+      />
+      <List />
     </div>
   );
 };
