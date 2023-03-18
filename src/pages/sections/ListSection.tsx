0592bd1405238fdfdf4bc8e0b64b2d70dcd4b74f
gharbiamine <gharbiamine5674@gmail.com>
2023-03-18 19:14:53 +0100

    added scroll to top + minor css fixes

--- src/pages/sections/ListSection.tsx
+++ src/pages/sections/ListSection.tsx
@@ -6,6 +6,7 @@ import { RepositoryNodeModel } from "../../models/RepositoryNodeModel";
 import { MoonLoader } from "react-spinners";
 import { filterRepositories } from "../../services/SearchService";
 import { Profile } from "../../components/Profile";
+import arrowUp from "../../assets/icons/arrow-up.svg";
 interface Props {
   isLoading: boolean;
 }
@@ -15,7 +16,23 @@ export const ListSection: FC<Props> = ({ isLoading }) => {
   const [repositoryList, SetRepositoryList] = useState<RepositoryNodeModel[]>(
     []
   );
-  const [value, setValue] = useState("");
+  const [showButton, setShowButton] = useState<boolean>(false);
+  const [value, setValue] = useState<string>("");
+  useEffect(() => {
+    const handleScrollButtonVisibility = () => {
+      window.scrollY > 300 ? setShowButton(true) : setShowButton(false);
+    };
+    window.addEventListener("scroll", handleScrollButtonVisibility);
+    return () => {
+      window.removeEventListener("scroll", handleScrollButtonVisibility);
+    };
+  }, []);
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      behavior: "smooth",
+      top: 0,
+    });
+  };
   useEffect(() => {
     SetRepositoryList(
       value
@@ -34,11 +51,11 @@ export const ListSection: FC<Props> = ({ isLoading }) => {
         <>
           {currentUser && (
             <>
-              <div className="flex h-screen overflow-y-scroll">
-                <div className="flex-shrink-0 flex flex-col justify-center items-center  w-1/3 md:sticky top-0 h-screen overflow-y-auto">
+              <div className="flex flex-col lg:flex-row h-full overflow-y-scroll  ">
+                <div className="flex-shrink-0 flex flex-col justify-center items-center lg:w-1/3 lg:sticky top-0 h-full lg:h-screen overflow-y-auto">
                   <Profile user={currentUser} />
                 </div>
-                <div className="flex-1 overflow-y-auto md:px-5 px-12 max-w-screen-xl">
+                <div className="flex-1 overflow-y-auto md:px-5 px-12 max-w-screen-xl max-h-screen">
                   <Search
                     handleSearch={(value: string) => {
                       setValue(value);
@@ -49,6 +66,16 @@ export const ListSection: FC<Props> = ({ isLoading }) => {
                 </div>
               </div>
             </>
+          )}
+          {showButton && (
+            <div className="fixed bottom-10 right-10">
+              <button
+                className=" bg-accent hover:shadow-lg text-overaccent font-bold p-2 rounded-full"
+                onClick={handleScrollToTop}
+              >
+                <img src={arrowUp} alt="go-up" />
+              </button>
+            </div>
           )}
         </>
       )}
