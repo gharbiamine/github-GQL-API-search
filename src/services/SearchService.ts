0c6473ee3ab6b77d15879407ce80b8dc4e986d33
gharbiamine <gharbiamine5674@gmail.com>
2023-11-10 20:04:20 +0100

    WIP: extracted backend + configured k8s deployment

--- src/services/SearchService.ts
+++ src/services/SearchService.ts
@@ -1,12 +1,6 @@
 import axios, { AxiosResponse } from "axios";
 import { RepositoryNodeModel } from "../models/RepositoryNodeModel";
 import { UserModel } from "../models/UserModel";
-import { getRepositoriesQuery } from "../schema/getRepositoriesQuery";
-
-axios.defaults.headers.common = {
-  Authorization: `Bearer ${import.meta.env.VITE_APP_GITHUB_PUBLIC_API_KEY}`,
-  "content-type": "application/json",
-};
 
 /**
 
@@ -30,15 +24,14 @@ interface UserModelResponse {
     @throws {Error} If the request times out.
     */
 export const getRepositories = async (username: string, limit = 100) => {
+  let data = JSON.stringify({
+    username: username,
+  });
   const options = {
     method: "POST",
     url: import.meta.env.VITE_APP_BASE_URL,
     data: {
-      query: getRepositoriesQuery,
-      variables: {
-        limit: limit,
-        username: username,
-      },
+      username: username,
     },
   };
 
@@ -50,9 +43,10 @@ export const getRepositories = async (username: string, limit = 100) => {
 
   const request = axios
     .request(options)
-    .then(
-      (response: AxiosResponse<UserModelResponse>) => response.data.data.user
-    );
+    .then((response: AxiosResponse<any>) => {
+      console.log(response.data.user);
+      return response.data.user;
+    });
 
   const result = await Promise.race([request, timeoutPromise]);
 
